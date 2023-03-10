946b626e6b85dabfadd588ba81bc8b4557998070
Dinh Le <dinh.le@tuni.fi>
2023-03-10 23:32:50 +0200

    done with desktop navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,21 @@
 import styles from './Navigation.module.scss';
-
+import coffeeLogo from "../../assets/icons/coffee.svg";
 import LoginGroup from './LoginGroup';
 
 const Navigation = () => {
     return (
         <nav className={styles.nav}>
-            <ul>
-                <li>MENU</li>
-                <li>ABOUT US</li>
-            </ul>
-            <LoginGroup />
+            <div className={styles.wrapper}>
+                <ul>
+                    <div className={styles.logo}>
+                        <img src={coffeeLogo} alt="coffee logo" />
+                    </div>
+                    <li>Menu</li>
+                    <li>About us</li>
+                    <li>Orders</li>
+                </ul>
+                <LoginGroup />
+            </div>
         </nav>
     );
 }
