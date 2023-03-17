66b61dbf90ef156763c654db1b09a5ed43461244
Dinh Le <dinh.le@tuni.fi>
2023-03-17 04:55:53 +0200

    adjust: a in Button flex center

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -7,6 +7,8 @@ import Button from '../UI/Button/Button';
 import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
 import coffee1 from "../../assets/img/coffee1.jpg";
 
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
 
     return (
@@ -15,7 +17,9 @@ const HomePage = () => {
                 <ContentWrapperText>
                     <h1>Fresh Coffee Delivered</h1>
                     <p>Our coffee shop provides you with a selection of expertly crafted coffee blends, roasted to perfection. Indulge in the comfort of your home and experience the perfect cup every time.</p>
-                    <Button className="dark text-and-icon"><DeliveryDiningIcon />Order now</Button>
+                    <Button className="dark text-and-icon">
+                        <Link to="/menu"><DeliveryDiningIcon />Order now</Link>
+                    </Button>
                 </ContentWrapperText>
                 <ContentWrapperImage src={coffee1} alt="Latte cup" />
             </ContentWrapper >
