83eb1e0e4c9b61c81bfc4ceb5074c3458fb95a7d
Dinh Le <dinh.le@tuni.fi>
2023-03-23 00:05:10 +0200

    add: require login to add to cart

--- src/components/DrinksMenu/DrinkCard.js
+++ src/components/DrinksMenu/DrinkCard.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../../contexts/user-context';
+
 import styles from './DrinkCard.module.scss';
 import dummy from "../../assets/img/dummy1.png";
 import Button from '../UI/Button/Button';
@@ -7,6 +9,23 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const DrinkCard = ({ drink }) => {
     const { name, description, price, image } = drink;
+    const { user } = useContext(UserContext);
+    const [cartMessage, setCartMessage] = useState(null);
+
+    const handleAddToCartClick = () => {
+        if (!user) {
+            setCartMessage('Please log in');
+            setTimeout(() => {
+                setCartMessage(null);
+            }, 2000);
+        } else {
+            setCartMessage('Item added to cart');
+            setTimeout(() => {
+                setCartMessage(null);
+            }, 2000);
+        }
+    }
+
 
     return (
         <div className={styles.card}>
@@ -16,7 +35,8 @@ const DrinkCard = ({ drink }) => {
                 <p className={styles.description}>{description}</p>
                 <p className={styles.price}>€{price.toFixed(2)}</p>
             </div>
-            <Button className='dark text-and-icon'><AddShoppingCartIcon />Add to Cart</Button>
+            <Button className='dark text-and-icon' onClick={handleAddToCartClick} disabled={cartMessage}><AddShoppingCartIcon />Add to Cart</Button>
+            {cartMessage && <div className={styles.message}>{cartMessage}</div>}
         </div >
     );
 };
